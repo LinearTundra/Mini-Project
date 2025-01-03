f524c0a36cfde4e899c2a7c9e51122187124e086
LinearTundra <samdarshmahajan123@gmail.com>
2025-01-03 15:35:03 +0530

    .

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
-
+const Cards = document.getElementsByClassName("Cards");
+const response = document.getElementById("Response");
 const SearchText = document.getElementById("Search");
-document.getElementsId("ResponseText").hidden=true;
+const fragment = document.createDocumentFragment();
 
-function search() {
-    const SearchText = document.getElementById("Search");
-    const searchbutton = document.getElementById("EARCH");
-    console.log(SearchText.value);
-    document.getElementById("SearchResultText").textContent = SearchText.value;
-   document.getElementsId("ResponseText").hidden=false;
-   }
+function rePosition() {
+    const SearchPos = SearchText.getBoundingClientRect();
+    response.style.top = `${SearchPos.bottom + 10}px`;
+    response.style.left = `${SearchPos.left}px`;
+}
+rePosition();
+window.addEventListener("resize", rePosition)
+
+SearchText.addEventListener("input", (event) => {
+    response.replaceChildren();
+    const textIn = event.target.value.toLowerCase();
+    for (let i = 0; i<Cards.length; i++) {
+        if (textIn == "") {
+            continue;
+        }
+        a = Cards[i].firstElementChild.childNodes[3].firstElementChild.firstElementChild.textContent;
+        b = Cards[i].firstElementChild.childNodes[3].firstElementChild.lastElementChild.textContent;
+        if (a.toLowerCase().includes(textIn) || b.toLowerCase().includes(textIn)) {
+            const duplicate = Cards[i].cloneNode(true);
+            fragment.appendChild(duplicate);
+        }
+    }
+    response.appendChild(fragment);
+})
